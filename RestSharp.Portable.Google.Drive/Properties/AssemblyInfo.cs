55146d5acb91add6ae0105b1d84cf13bafaec90a
Mark Junker <fubar-coder@live.de>
2015-10-19 10:11:54 +0200

    Bumped version

--- RestSharp.Portable.Google.Drive/Properties/AssemblyInfo.cs
+++ RestSharp.Portable.Google.Drive/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Reflection;
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.6.*")]
-[assembly: AssemblyFileVersion("0.6.0.3")]
-[assembly: AssemblyInformationalVersion("0.6.0-beta03")]
+[assembly: AssemblyFileVersion("0.6.0.4")]
+[assembly: AssemblyInformationalVersion("0.6.0-beta04")]
